75503db6580023eb959f5edc4e62973141f73fef
dontech09 <donnjondanmakal@gmail.com>
2022-05-26 12:17:43 +0530

    CSS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import FileBase64 from "react-file-base64";
+import { useState } from "react";
+import axios from "axios";
 function App() {
+  const [data, setData] = useState("");
+
+  function evaluate(x) {
+    const data = {
+      image: x,
+      other_key: "value",
+    };
+    setData("Evaluating the Image....");
+    axios({
+      method: "post",
+      url: "https://workshop-api-app.herokuapp.com/predict",
+      data,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "text/plain",
+      },
+    })
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data.result);
+      })
+      .catch((error) => {
+        setData("Something went wrong");
+      });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="/logo.png" />
+        <h1>Tinkerhub CET</h1>
+        <h2> Separating non-biodegradable solid waste</h2>
+
+        <FileBase64
+          class="custom-file-input"
+          type="image"
+          multiple={false}
+          onDone={(base64) => {
+            console.log(base64.base64.split(",")[1], "Just Testing");
+            evaluate(base64.base64.split(",")[1]);
+          }}
+        />
+        <p>{data}</p>
       </header>
     </div>
   );
